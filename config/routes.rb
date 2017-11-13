3d9bb6feda2279228e53583bed680d9449d9f9d2
acartesc <acartescc@gmail.com>
2017-11-13 20:12:39 -0300

    creating users ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+
 end
